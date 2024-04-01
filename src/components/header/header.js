b7dfcfbff952fd654c190f00a0b6e57a37fe5c25
Elenadelatorre <elenadelatorre1806@gmail.com>
2024-04-01 17:53:33 +0200

    Cambios responsive

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,5 +3,5 @@ import './header.css';
 export const printHeader = () => {
   document.querySelector(
     'header'
-  ).innerHTML = `<section id="header"><h1>HAVE FUN WITH ME!</h1></section>`;
+  ).innerHTML = `<section id="header"><h1>ENJOY WITH FRIENDS!</h1></section>`;
 };
